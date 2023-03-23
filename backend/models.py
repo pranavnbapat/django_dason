0a06afe72c1f39040228c0560f14630f38ca3f95
pranavnbapat <pranav.g33k@gmail.com>
2023-03-23 17:22:19 +0100

    django form validation in progress

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class MyForm(models.Model):
@@ -7,11 +8,15 @@ class MyForm(models.Model):
 
     id = models.AutoField(primary_key=True, db_column='id', db_index=True, editable=False, unique=True,
                           blank=False, null=False, verbose_name='ID')
-    fname = models.CharField(max_length=50, null=False, blank=False)
-    lname = models.CharField(max_length=50, null=False, blank=False)
+    fname = models.CharField(max_length=50, null=False, blank=False,
+                             validators=[RegexValidator(r'^[a-zA-Z\s]+', message="Invalid characters")])
+    lname = models.CharField(max_length=50, null=False, blank=False,
+                             validators=[RegexValidator(r'^[a-zA-Z\s]+', message="Invalid characters")])
     email = models.EmailField(max_length=50, unique=True, null=False, blank=False, db_index=True)
-    gender = models.CharField(max_length=1, null=False, blank=False)
-    dob = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=1, null=False, blank=False,
+                              validators=[RegexValidator(r'^[a-zA-Z]+', message="Invalid characters")])
+    dob = models.DateField(null=False, blank=False,
+                           validators=[RegexValidator(r'^[0-9-]+', message="Invalid characters")])
     status = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(null=True, blank=True)
--- backend/views.py
+++ backend/views.py
@@ -1,25 +1,20 @@
-from django.contrib.auth import get_user_model
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import MyForm
-from django.views.decorators.http import require_POST
 
 
 class FormView(TemplateView):
     template_name = "backend/forms/form.html"
-    success_url = reverse_lazy("backend:my_form")
 
-    def get(self, request, *args, **kwargs):
-        # Pass the form data in the context if it exists
-        context = self.get_context_data(**kwargs)
-        print(context)
-        context['form_data'] = request.POST.copy()
-        print(context)
-        # context['form_data'] = request.POST if 'form_data' in request.POST else None
-        return self.render_to_response(context)
+    post_data = {}
+
+    # Populate fields on page load
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['post_data'] = self.post_data
+    #     return context
 
     def post(self, request):
         fname = request.POST.get('fname')
@@ -28,28 +23,17 @@ class FormView(TemplateView):
         gender = request.POST.get('gender')
         dob = request.POST.get('dob')
 
-        # Print post data
-        # for key, value in request.POST.items():
-        #     print('Key: %s' % (key))
-        #     # print(f'Key: {key}') in Python >= 3.7
-        #     print('Value %s' % (value))
-        #     # print(f'Value: {value}') in Python >= 3.7
-
-        # Validate form inputs
-        # if not fname or not lname or not email or not gender or not dob:
-        #     messages.error(request, 'All fields are required.')
-        #     form_data = request.POST.copy()
-        #     return render(request, self.template_name, {'form_data': form_data})
+        self.post_data = request.POST.dict()
 
         try:
             form_data = MyForm(fname=fname, lname=lname, email=email, gender=gender, dob=dob)
+            form_data.full_clean()
             form_data.save()
             messages.success(request, "Form data saved successfully.")
             return redirect('backend:my_form')
         except Exception as e:
-            # print(e)
             messages.error(request, e)
-            return redirect('backend:my_form')
+            return render(request, 'backend/forms/form.html', {'post_data': self.post_data})
 
 
 class ProfileView(TemplateView):
@@ -61,4 +45,3 @@ form_view = login_required(FormView.as_view())
 
 # User profile
 profile_view = login_required(ProfileView.as_view())
-
